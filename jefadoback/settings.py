38f53bba4a0a8b29b65894007c7610671ea8ac53
marvidik <ebubeidika@gmail.com>
2026-07-05 10:58:07 +0100

    changed logout timer

--- jefadoback/settings.py
+++ jefadoback/settings.py
@@ -199,8 +199,8 @@ REST_AUTH = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
-    'REFRESH_TOKEN_LIFETIME': timedelta(hours=72),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=3),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
